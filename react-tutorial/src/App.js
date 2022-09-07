933be3ecbfef52f04a64987313346353a23ed234
VirginieBoissiere <boissiere.virginie@gmail.com>
2022-09-07 17:11:25 +0200

    ajout du boutton delete et mise en fonction avec onclick

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,35 +1,51 @@
 import React,{Component} from 'react'
 import Table from './Table'
+
 class App extends Component {
-    render() {
-      const characters = [
+    state = {
+      characters: [
         {
           name: 'Charlie',
-          job: 'Janitor',
-        },
-        {
-          name: 'Mac',
-          job: 'Bouncer',
-        },
-        {
-          name: 'Dee',
-          job: 'Aspring actress',
-        },
-        {
-          name: 'Dennis',
-          job: 'Bartender',
-        },
-        {
-            name: 'Virginie',
-            job: 'Dev',
-          },
-      ]
+              job: 'Janitor',
+            },
+            {
+              name: 'Mac',
+              job: 'Bouncer',
+            },
+            {
+              name: 'Dee',
+              job: 'Aspring actress',
+            },
+            {
+              name: 'Dennis',
+              job: 'Bartender',
+            },
+            {
+                name: 'Virginie',
+                job: 'Dev',
+              },
+          ]
+        }
+        
+        removeCharacter = (index) => {
+            const { characters } = this.state
+            
+            this.setState({
+                characters: characters.filter((character, i) => {
+                    return i !== index
+                }),
+            })
+        }
+        render() {
+            
+            const { characters } = this.state
+            
+            return (
+                <div className="container">
+      <Table characterData={characters} removeCharacter={this.removeCharacter} />
+    </div>
+  )
+}
+}
   
-      return (
-        <div className="container">
-          <Table characterData={characters} />
-        </div>
-      )
-    }
-  }
 export default App
